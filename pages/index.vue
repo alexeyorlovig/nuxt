7e00b5d43feaa94192896892f3b120160a7d4a8e
alexeyorlovig <alexeyorlovig@mail.ru>
2018-03-08 14:51:01 +0300

    v 1.0

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page">
+    <div>Привет я буду серым цветом!</div>
+    <input type="text" name="" value="" placeholder="Привет">
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style lang="stylus">
+</style>
